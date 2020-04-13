40e16984c1f12d609643afb97a4f2e12b2378c00
JinZhuXing <peace1019@126.com>
2020-04-13 22:57:16 +0800

    add histogram stretch for prepare dataset

--- source/01_preprocess/01_03_prepare_dataset.py
+++ source/01_preprocess/01_03_prepare_dataset.py
@@ -16,6 +16,47 @@ def parse_file_name2(img_path):
     #print(user_id, finger_id)
     return np.array([user_id], dtype=np.uint16)
 
+def hist_stretch(img_buf, width, height, shift):
+    tmp1 = 0
+    tmp2 = 0
+    w_pHistBuf = np.zeros(256, dtype=np.uint32)
+    total = 0
+    ret_buf = np.zeros((width, height, 1), dtype=np.uint8)
+
+    # get brightness
+    for i in range(height):
+        for j in range(width):
+            total = total + img_buf[i][j][0]
+            w_pHistBuf[img_buf[i][j][0]] += 1
+    diff = (int)(shift - (total / (width * height)))
+    w_pHistBuf = np.zeros(256, dtype=np.uint32)
+
+    # move histogram
+    for i in range(height):
+        for j in range(width):
+            tmp = img_buf[i][j][0] + diff
+            if (tmp > 255):
+                tmp = 255
+            elif (tmp < 0):
+                tmp = 0
+            ret_buf[i][j][0] = tmp
+            w_pHistBuf[tmp] += 1
+
+    # stretch histogram
+    for i in range(256):
+        if (w_pHistBuf[i] != 0):
+            tmp1 = i
+            break
+    for i in range(255, 0, -1):
+        if (w_pHistBuf[i] != 0):
+            tmp2 = i
+            break
+    for i in range(height):
+        for j in range(width):
+            ret_buf[i][j][0] = (int)((255 * (ret_buf[i][j][0] - tmp1) / (tmp2 - tmp1)))
+    
+    return ret_buf
+
 
 
 # %%
@@ -27,8 +68,10 @@ imgs = np.empty((len(img_list), 160, 160, 1), dtype = np.uint8)
 labels = np.empty((len(img_list), 1), dtype = np.uint16)
 
 for i, img_path in enumerate(img_list):
+    print(i)
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    imgs[i] = img.reshape(160, 160, 1)
+    tmp_img = img.reshape(160, 160, 1)
+    imgs[i] = hist_stretch(tmp_img, 160, 160, 128)
 
     # get user id
     labels[i] = parse_file_name2(img_path)
@@ -45,8 +88,10 @@ imgs = np.empty((len(img_list), 160, 160, 1), dtype = np.uint8)
 labels = np.empty((len(img_list), 1), dtype = np.uint16)
 
 for i, img_path in enumerate(img_list):
+    print(i)
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    imgs[i] = img.reshape(160, 160, 1)
+    tmp_img = img.reshape(160, 160, 1)
+    imgs[i] = hist_stretch(tmp_img, 160, 160, 128)
 
     # get user id
     labels[i] = parse_file_name(img_path)
