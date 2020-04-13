29fbb21d3fad090d1d39391f36399a2d21603b9d
JinZhuXing <peace1019@126.com>
2020-04-13 10:38:33 +0800

    add show graph for train

--- source/02_train/02_01_train_data.py
+++ source/02_train/02_01_train_data.py
@@ -182,6 +182,31 @@ history = model.fit(train_gen, epochs = 100, validation_data = val_gen, callback
 
 
 # %%
+# show result graph
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+
+# %%
 # save model
 model_path = '../../model/result/fp160.h5'
 model_feature_path = '../../model/result/fp160_feature.h5'
